15beb21a68cd9722d0133df9cfd91200aa4cafd6
Prateek Mahajan <prateekmahajan04@gmail.com>
2025-09-16 04:57:46 +0530

    simple code

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,4 +1,10 @@
-module "rg" {
-  source = "../../module/rg"
-  rg     = var.rg
+# module "rg" {
+#   source = "../../module/rg"
+#   rg     = var.rg
+# }
+
+resource "azurerm_resource_group" "rg" {
+  # for_each = var.rg
+  name     = "rg1"
+  location = "eastus"
 }
