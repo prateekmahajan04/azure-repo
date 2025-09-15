aa795ce2c9fedb395d6787b52a74d39ba0cd1f92
Prateek Mahajan <prateekmahajan04@gmail.com>
2025-09-16 02:08:29 +0530

    lets start with RG

---
+++ env/dev/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.44.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    
+  }
+  subscription_id = "3bcdc0e9-b6e4-470c-86a7-c19f6b5069c8"
+}
