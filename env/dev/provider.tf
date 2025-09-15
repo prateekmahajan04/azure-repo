09c914b8064a54d3a97a2503e5ae694dc1d3f04c
Prateek Mahajan <prateekmahajan04@gmail.com>
2025-09-16 05:15:24 +0530

    backendremoved

--- env/dev/provider.tf
+++ env/dev/provider.tf
@@ -6,18 +6,17 @@ terraform {
     }
   }
 
+#   backend "azurerm" {
+#     resource_group_name  = "prateektfstatebackend"
+#     storage_account_name = "prateektfstatebackendstg" # must be globally unique
+#     container_name       = "tfstate"
+#     key                  = "terraform.tfstate"
+#   }
+# }
 
-  backend "azurerm" {
-    resource_group_name  = "prateektfstatebackend"
-    storage_account_name = "prateektfstatebackendstg" # must be globally unique
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
 }
 
 provider "azurerm" {
-  features {
-
-  }
+  features {}
   subscription_id = "3bcdc0e9-b6e4-470c-86a7-c19f6b5069c8"
 }
