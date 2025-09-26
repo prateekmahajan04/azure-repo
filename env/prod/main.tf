f3e3ab2a704d9567ddf3bfe3116fb0a786616134
Prateek Mahajan <prateekmahajan04@gmail.com>
2025-09-26 11:36:57 +0530

    rr

--- env/prod/main.tf
+++ env/prod/main.tf
@@ -1,6 +1,13 @@
 module "rg" {
   source = "../../module/rg"
-  rg     = var.rg
+
+  rg = {
+    for i in range(6, var.rg_count+6) :
+    "rg${i}" => {
+      name     = "${var.rg_prefix}${i}"
+      location = var.location
+    } if !(contains(var.rg_exclude, i))
+  }
+
   tags = var.tags
 }
-
--- env/prod/variable.tf
+++ env/prod/variable.tf
@@ -6,3 +6,6 @@ variable "tags" {
   description = "Common tags for all resources"
   type        = map(string)
 }
+variable "rg_exclude" {
+  
+}
