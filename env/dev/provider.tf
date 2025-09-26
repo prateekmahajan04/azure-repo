165d7d7bda840f1525f7c86099af91096d251a39
Prateek Mahajan <prateekmahajan04@gmail.com>
2025-09-26 09:49:12 +0530

    update

--- env/dev/provider.tf
+++ env/dev/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "prateektfstatebackend"
-    storage_account_name = "prateektfstatebackendstg" # must be globally unique
+    storage_account_name = "prateekbackendstrg" # must be globally unique
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
   }
@@ -17,5 +17,5 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "3bcdc0e9-b6e4-470c-86a7-c19f6b5069c8"
+  subscription_id = "e5776277-6d0c-4f2c-97d3-a378ac7502d6"
 }
